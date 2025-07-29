3ef2f0fb1b5a3753f65fb5016b2db0d1911ed098
Wallace027Dev <wallaceofc@hotmail.com>
2025-07-29 16:52:21 -0300

    final

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const pokemonSection = document.getElementById("pokemon-section");
 
 function adicionarNovoPokemon(pokemon) {
   const tipos = pokemon.types.map((tipo) => tipo.type);
-  console.log(tipos)
 
   pokemonSection.innerHTML += `
     <div class="pokemon-card">
@@ -16,72 +15,35 @@ function adicionarNovoPokemon(pokemon) {
   `;
 
   const tiposDiv = document.getElementById(`tipos-${pokemon.name}`);
-  tipos.map(tipo => tiposDiv.innerHTML += `<span class="${tipo.name}">${tipo.name}</span>`)
+  tipos.map(
+    (tipo) =>
+      (tiposDiv.innerHTML += `<span class="${tipo.name}">${tipo.name}</span>`)
+  );
 }
 
-const pokemons = [
-  {
-    id: 1,
-    name: "bulbasaur",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-    },
-    types: [
-      { type: { name: "grass" } },
-      { type: { name: "poison" } }
-    ]
-  },
-  {
-    id: 4,
-    name: "charmander",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-    },
-    types: [
-      { type: { name: "fire" } }
-    ]
-  },
-  {
-    id: 7,
-    name: "squirtle",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-    },
-    types: [
-      { type: { name: "water" } }
-    ]
-  },
-  {
-    id: 25,
-    name: "pikachu",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-    },
-    types: [
-      { type: { name: "electric" } }
-    ]
-  },
-  {
-    id: 39,
-    name: "jigglypuff",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png"
-    },
-    types: [
-      { type: { name: "normal" } },
-      { type: { name: "fairy" } }
-    ]
-  },
-  {
-    id: 150,
-    name: "mewtwo",
-    sprites: {
-      front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"
-    },
-    types: [
-      { type: { name: "psychic" } }
-    ]
-  }
-];
+const URL = "https://pokeapi.co/api/v2";
 
-pokemons.forEach(adicionarNovoPokemon)
+async function buscarPokemons() {
+  const resposta = await fetch(URL + "/pokemon&limit=20");
+
+  const dados = await resposta.json();
+  console.log(dados);
+
+  const pokemons = dados.results;
+
+  const todos = await Promise.all(
+    pokemons.map(async (pokemon) => {
+      const resposta = await fetch(pokemon.url);
+
+      const dados = await resposta.json();
+
+      return dados;
+    })
+  );
+
+  return todos;
+}
+
+const pokemons = buscarPokemons().then((pokemons) => {
+  pokemons.forEach(adicionarNovoPokemon);
+});
